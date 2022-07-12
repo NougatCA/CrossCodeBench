2ffe1e0cc678147dbe2657ec4c11e8a7c87dd708
NougatCA <nougatca@outlook.com>
2022-07-12 16:20:47 +0800

    Update changes

--- src/benchmark/args.py
+++ src/benchmark/args.py
@@ -6,10 +6,10 @@ import configs
 
 def add_args(parser: ArgumentParser):
 
-    # model identifier
-    parser.add_argument("--model", type=str, default="codebert",
+    # model
+    parser.add_argument("--init_model", type=str, default="codet5",
                         choices=configs.MODEL_ID_TO_NAMES.keys(),
-                        help="Model identifier.")
+                        help="Initialization model.")
 
     # task, dataset and subset
     parser.add_argument("--task", type=str, default="defect",
@@ -25,8 +25,6 @@ def add_args(parser: ArgumentParser):
     # train, valid and test procedure
     parser.add_argument("--only_test", action="store_true", default=False,
                         help="Whether to only perform testing procedure.")
-    parser.add_argument("--do_not_valid", action="store_true", default=False,
-                        help="Do not do validation after each epoch.")
 
     # hyper parameters
     parser.add_argument("--num_epochs", type=int, default=None,
@@ -93,120 +91,6 @@ def add_args(parser: ArgumentParser):
     parser.add_argument("--wandb_mode", type=str, default="online",
                         choices=["online", "offline", "disabled"],
                         help="Set the wandb mode.")
-
-
-def set_task_hyper_parameters(args):
-
-    num_epochs = 30
-    train_batch_size = 32
-    eval_batch_size = 32
-    max_source_length = 256
-    max_source_pair_length = 256
-    max_target_length = 256
-    learning_rate = 5e-5
-    num_warmup_steps = 1000
-    patience = 5
-
-    if args.task == "defect":
-        num_epochs = 20
-        max_source_length = 512
-        max_target_length = 3
-        patience = 5
-        learning_rate = 2e-5
-
-    elif args.task == "clone":
-        num_epochs = 1
-        max_source_length = 256
-        max_source_pair_length = 256
-        max_target_length = 3
-        patience = 2
-
-    elif args.task == "exception":
-        num_epochs = 20
-        max_source_length = 256
-        max_target_length = 10
-        patience = 5
-
-    elif args.task == "retrieval":
-        num_epochs = 20
-        max_source_length = 512
-        max_target_length = 512
-        patience = 5
-
-    elif args.task == "search":
-        num_epochs = 20
-        max_source_length = 256
-        max_target_length = 256
-        patience = 5
-
-    elif args.task == "cosqa":
-        num_epochs = 5
-        learning_rate = 5e-6
-        num_warmup_steps = 500
-        max_source_length = 256
-
-    elif args.task == "translation":
-        num_epochs = 50
-        max_source_length = 384
-        max_target_length = 256
-        patience = 5
-
-    elif args.task == "fixing":
-        num_epochs = 50
-        if args.sub_task == "small":
-            max_source_length = 128
-            max_target_length = 128
-        elif args.sub_task == "medium":
-            max_source_length = 256
-            max_target_length = 256
-        patience = 5
-
-    elif args.task == "mutant":
-        num_epochs = 50
-        max_source_length = 128
-        max_target_length = 128
-        patience = 5
-
-    elif args.task == "assert":
-        num_epochs = 30
-        if args.sub_task == "abs":
-            max_source_length = 512
-            max_target_length = 64
-        elif args.sub_task == "raw":
-            max_source_length = 256
-            max_target_length = 32
-        patience = 5
-
-    elif args.task == "summarization":
-        num_epochs = 15
-        max_source_length = 256
-        max_target_length = 128
-        patience = 3
-
-    elif args.task == "generation":
-        num_epochs = 30
-        max_source_length = 256
-        max_target_length = 128
-        patience = 5
-
-    if args.max_source_length is None:
-        args.max_source_length = max_source_length
-    if args.max_target_length is None:
-        args.max_target_length = max_target_length
-    if args.max_source_pair_length is None:
-        args.max_source_pair_length = max_source_pair_length
-    if args.num_epochs is None:
-        args.num_epochs = num_epochs
-    if args.train_batch_size is None:
-        args.train_batch_size = train_batch_size
-    if args.eval_batch_size is None:
-        args.eval_batch_size = eval_batch_size
-    if args.learning_rate is None:
-        args.learning_rate = learning_rate
-    if args.num_warmup_steps is None:
-        args.num_warmup_steps = num_warmup_steps
-    if args.patience is None:
-        args.patience = patience
 
 
 def check_args(args):
--- src/benchmark/configs.py
+++ src/benchmark/configs.py
@@ -1,2 +1,4 @@
 
+# init model
+init_models
 
